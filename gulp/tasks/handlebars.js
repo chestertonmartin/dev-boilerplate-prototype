fc9f30f692ec6f5ab98bf16b6fed47892a4a2a45
msorensson <martin.sorensson@creuna.se>
2016-04-08 14:16:03 +0200

    added data to build process

--- gulp/tasks/handlebars.js
+++ gulp/tasks/handlebars.js
@@ -1,19 +1,40 @@
 var gulp = require('gulp'),
     handlebars = require('gulp-compile-handlebars'),
     layouts = require('handlebars-layouts'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    fs = require('fs');
 
 var options = {
     ignorePartials: true,
     batch : ['./src/markup/partials'],
     helpers: {}
-};
+},
+    templateDataDirectory = './src/data';
+
 
 handlebars.Handlebars.registerHelper(layouts(handlebars.Handlebars));
 
+function getTemplateData() {
+    var data= {},
+        files,
+        file;
+
+    files = fs.readdirSync(templateDataDirectory);
+
+    files.forEach(function(file) {
+        if (file !== '.DS_Store') {
+            data[file.replace('.json', '')] = JSON.parse(fs.readFileSync(templateDataDirectory + '/' + file, 'utf-8'));
+        }
+    });
+
+    return data;
+}
+
 gulp.task('handlebars', function() {
+    var templateData = getTemplateData();
+
     return gulp.src('./src/markup/templates/*.hbs')
-        .pipe(handlebars(null, options))
+        .pipe(handlebars(templateData, options))
         .pipe(rename(function(path) {
             path.extname = '.html';
         }))
